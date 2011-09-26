c1e5ec881d468cf0e763f600d149eb9b013c3675
vinjn <vinjn.z@gmail.com>
2011-09-26 15:24:21 +0800

    update SimpleMutex.h

--- _common/SimpleMutex.h
+++ _common/SimpleMutex.h
@@ -2,7 +2,6 @@
 
 #include <windows.h>
 
-
 class CSimpleMutex: public CRITICAL_SECTION
 {
 public:
@@ -17,12 +16,12 @@ public:
         ::DeleteCriticalSection(this);
     }
 
-    void Lock ()
+    void lock ()
     {
         ::EnterCriticalSection(this);
         m_nLockCount++;
     }
-    void UnLock ()
+    void unlock ()
     {
         ::LeaveCriticalSection(this);
         m_nLockCount--;
@@ -31,4 +30,55 @@ public:
 
 public :
     int m_nLockCount;
+};
+
+struct ScopedLocker
+{
+	CSimpleMutex& _mutex;
+	ScopedLocker(CSimpleMutex& mutex):_mutex(mutex)
+	{
+		_mutex.lock();
+	}
+	~ScopedLocker()
+	{
+		_mutex.unlock();
+	}
+};
+
+/*usage
+CSimpleMutex a_mutex;
+
+{
+ScopedLocker(a_mutex);
+//do something
+}
+*/
+#define LOCK_START(mutex) {ScopedLocker _locker(mutex)
+#define LOCK_END() }
+
+class CSimpleEvent
+{
+public:
+	CSimpleEvent ()
+	{
+		// start in non-signaled state (red light)
+		// auto reset after every Wait
+		_handle = CreateEvent (0, FALSE, FALSE, 0);
+	}
+
+	~CSimpleEvent ()
+	{
+		CloseHandle (_handle);
+	}
+
+	// put into signaled state
+	void set () { SetEvent (_handle); }
+	void wait ()
+	{
+		// Wait until event is in signaled (green) state
+		WaitForSingleObject (_handle, INFINITE);
+	}
+	operator HANDLE () { return _handle; }
+private:
+	HANDLE _handle;
 };
